77e5fa8087a30651628ab18d59168a77c556b263
likely <532837139@qq.com>
2016-12-27 18:11:30 +0800

    1:topbar min click

--- src/components/topNavbar.js
+++ src/components/topNavbar.js
@@ -1,10 +1,38 @@
 import React,{} from 'react'
 
-const topNavbar=()=>(
-	<div className="row border-bottom">
+class topNavbar extends React.Component{
+	SmoothlyMenu() {
+    if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
+        // Hide menu in order to smoothly turn on when maximize menu
+        $('#side-menu').hide();
+        // For smoothly turn on menu
+        setTimeout(
+            function () {
+                $('#side-menu').fadeIn(500);
+            }, 100);
+    } else if ($('body').hasClass('fixed-sidebar')) {
+        $('#side-menu').hide();
+        setTimeout(
+            function () {
+                $('#side-menu').fadeIn(500);
+            }, 300);
+    } else {
+        // Remove all inline style from jquery fadeIn function to reset menu state
+        $('#side-menu').removeAttr('style');
+    }
+}
+
+	handleMinClick(){
+		$("body").toggleClass("mini-navbar")
+		this.SmoothlyMenu()
+	}
+
+	render(){
+		return(
+			<div className="row border-bottom">
         <nav className="navbar navbar-static-top white-bg" role="navigation" >
         <div className="navbar-header">
-            <a className="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i className="fa fa-bars"></i> </a>
+            <a className="navbar-minimalize minimalize-styl-2 btn btn-primary " onClick={r=>{this.handleMinClick()}} href="#"><i className="fa fa-bars"></i> </a>
             <form role="search" className="navbar-form-custom" action="search_results.html">
                 <div className="form-group">
                     <input type="text" placeholder="Search for something..." className="form-control" name="top-search" id="top-search" />
@@ -126,6 +154,8 @@ const topNavbar=()=>(
 
         </nav>
         </div>
-)
+		)
+	}
+}
 
 export default topNavbar
