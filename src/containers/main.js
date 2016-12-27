c7db9a6bf401f6976574974140379a5fd71868fe
likely <532837139@qq.com>
2016-12-27 17:30:41 +0800

    1:导航栏移植

--- src/containers/main.js
+++ src/containers/main.js
@@ -1,11 +1,16 @@
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import React, { PropTypes } from 'react'
+import Navigation from '../components/navigation'
+import RightSliderbar from '../components/rightSidebar'
+import TopNavbar from '../components/topNavbar'
 
 const Main = ({}) => (
-	<div>
-		<h1>main</h1>
-		
+	<div id="wrapper">
+			<Navigation />
+			<div id="page-wrapper" className="gray-bg">
+				<TopNavbar />
+    	</div>
 	</div>
 )
 
