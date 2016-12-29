d94083e3fba88e69044dae0afb4e7589db716e79
likely <532837139@qq.com>
2016-12-29 19:18:03 +0800

    用户列表mock

--- mock/mocks/user_list.js
+++ mock/mocks/user_list.js
@@ -2,7 +2,23 @@ module.exports = function() {
   var list = {
     "code": 0,
     "msg": null,
-    "data": null
+    "data": {
+      current: 3,
+      totalPages: 7,
+      data: [{
+        id: '873218372jshadhad',
+        username: 'Likely',
+        email: '532837139@qq.com',
+        emailVerifyed: true,
+        amount: 10000.00
+      }, {
+        id: '873218372jshadhad',
+        username: 'Likely',
+        email: '532837139@qq.com',
+        emailVerifyed: true,
+        amount: 10000.00
+      }]
+    }
   }
 
   return list
