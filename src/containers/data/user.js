9b7a799a1f9f60fa9348d93945b3ccbbae6751c9
likely <532837139@qq.com>
2016-12-29 19:19:03 +0800

    拉取列表function

--- src/containers/data/user.js
+++ src/containers/data/user.js
@@ -2,9 +2,10 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import React, { PropTypes } from 'react'
 import Table from 'Components/table'
+import { fetchList } from 'actions/list'
 
-const user = ({ headers,data,totalPages,currentPage }) => (
-	<Table headers={headers}  data={data} totalPages={totalPages} currentPage={currentPage} title='用户' />
+const user = ({ headers,data,totalPages,currentPage,fetchList }) => (
+	<Table headers={headers}  data={data} totalPages={totalPages} currentPage={currentPage} title='用户' fetchList={fetchList} />
 )
 
 user.propTypes = {
